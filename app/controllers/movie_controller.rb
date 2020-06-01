083e9601b2e3d2aaeb5b31e1e19f72ebe17b9b24
Alex Velasquez <jtinnesco@gmail.com>
2020-06-01 20:14:06 +0000

    add @user and @movies global variables to access users movies in index.erb

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -2,6 +2,9 @@ class MovieController < ApplicationController
 
   get '/movies' do
     if logged_in?
+      @user = current_user
+      @movies = @user.movies.all
+      binding.pry
       erb :'/movies/index'
     else
       redirect '/login'
