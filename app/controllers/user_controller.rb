bf628898c6fa8a56965b7cc39b2df472e54a5905
Alex Velasquez <jtinnesco@gmail.com>
2020-06-01 20:05:56 +0000

    add user save and login to post signup

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -14,10 +14,14 @@ class UserController < ApplicationController
     if logged_in?
       redirect '/movies'
     else
-
-
+      @user = User.create(username: params[:username], password: params[:password])
+      if @user.save
+        session[:user_id] = @user.id #sets session id, logs user in
+        redirect '/movies'
+      else
+        redirect '/signup'
+      end
     end
-
   end
 
   #post signup
