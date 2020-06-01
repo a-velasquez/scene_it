8164a4804e56906d99164b60b3d0cc7f18318897
Alex Velasquez <jtinnesco@gmail.com>
2020-06-01 18:23:39 +0000

    create user_controller added signup route

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,27 @@
+class UserController < ApplicationController
+
+  #signup
+
+  get '/signup' do
+    erb :'users/signup'
+  end
+
+
+  #get signup
+
+
+
+
+  #post signup
+
+
+
+
+  #login
+
+
+
+  #logout
+
+
+end
