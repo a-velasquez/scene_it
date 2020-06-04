a812aeb157c2bc6121f3b7b0d5c481a9b28b7e42
Alex Velasquez <jtinnesco@gmail.com>
2020-06-04 22:52:14 +0000

    add self.valid_params? method to validate params from user

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,7 @@
 class Movie < ActiveRecord::Base
   belongs_to :user
 
-  validates :title, presence: true
-  validates :genre, presence: true
-  validates :release_date, presence: true 
+  def self.valid_params?(params)
+    return !params[:title].empty? && !params[:genre].empty? && !params[:release_date].empty? && !params[:description].empty? && !params[:rating].empty?
+  end
 end
