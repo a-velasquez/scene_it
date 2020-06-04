962fbd80c803fea2add783f92f97b7cf663f614d
Alex Velasquez <jtinnesco@gmail.com>
2020-06-04 05:47:41 +0000

    add active record validations to Movie model, validates title, genre and release date

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,7 @@
 class Movie < ActiveRecord::Base
-  belongs_to :user 
+  belongs_to :user
+
+  validates :title, presence: true
+  validates :genre, presence: true
+  validates :release_date, presence: true, length: { is: 4 }, numericality: { only_integer: true } 
 end
