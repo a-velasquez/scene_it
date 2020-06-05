743f5b235da166a2ac50eb60b89d0bc51cdca929
Alex Velasquez <jtinnesco@gmail.com>
2020-06-05 02:29:06 +0000

    use authorized? method in delete route to check if movie belongs to current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -26,6 +26,10 @@ class ApplicationController < Sinatra::Base
       @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
     end
 
+    def authorized?(movie)
+      movie.user == current_user
+    end
+
   end
 
 end
