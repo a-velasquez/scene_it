1df36c93212eeb0e6c5f9bd170fae2773eedd0de
Alex Velasquez <jtinnesco@gmail.com>
2020-06-09 03:44:00 +0000

    add set_session_id method to post /login route

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -37,7 +37,7 @@ class UserController < ApplicationController
 
   post '/login' do
     @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password]) #uses Activerecord method to authenticate users password
+    if @user && @user.authenticate(params[:password]) #uses Activerecord method to authenticate user's password
       set_session_id
       redirect '/movies'
     else
