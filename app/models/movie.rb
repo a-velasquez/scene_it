1c59700ef0363d36d120ff14718551c5378b4257
Alex Velasquez <jtinnesco@gmail.com>
2020-06-14 05:19:46 +0000

    add active record validations to Movie model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,7 +1,9 @@
 class Movie < ActiveRecord::Base
   belongs_to :user
 
-  def self.valid_params?(params)
-    return !params[:title].empty? && !params[:genre].empty? && !params[:release_date].empty? && !params[:description].empty? && !params[:rating].empty?
-  end
+  validates :title, :genre, :release_date, :description, :rating, presence: true
+
+  # def self.valid_params?(params)
+  #   return !params[:title].empty? && !params[:genre].empty? && !params[:release_date].empty? && !params[:description].empty? && !params[:rating].empty?
+  # end
 end
