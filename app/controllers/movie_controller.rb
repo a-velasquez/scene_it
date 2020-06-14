89759339557db3f60250ab79803041ceca2f9c20
Alex Velasquez <jtinnesco@gmail.com>
2020-06-14 05:22:43 +0000

    refactor post /movies route to rely on active record validations to validate user params as opposed to helper method

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -11,18 +11,18 @@ class MovieController < ApplicationController
 
   post '/movies' do
     if logged_in?
-      if @movie = Movie.valid_params?(params) #validates user inputs before creating movie
-        @movie = current_user.movies.create(
-          title: params[:title],
-          genre: params[:genre],
-          release_date: params[:release_date],
-          description: params[:description],
-          rating: params[:rating]
-          )
-          redirect "/movies/#{@movie.id}"
-        else
-          redirect '/movies/new'
-        end
+      @movie = current_user.movies.create(
+        title: params[:title],
+        genre: params[:genre],
+        release_date: params[:release_date],
+        description: params[:description],
+        rating: params[:rating]
+        )
+      if @movie.save
+        redirect "/movies/#{@movie.id}"
+      else
+        redirect '/movies/new'
+      end
     else
       redirect '/login'
     end
@@ -110,3 +110,41 @@ class MovieController < ApplicationController
   end
 
 end
+
+# post '/movies' do
+#   if logged_in?
+#     if @movie = Movie.valid_params?(params) #validates user inputs before creating movie
+#       @movie = current_user.movies.create(
+#         title: params[:title],
+#         genre: params[:genre],
+#         release_date: params[:release_date],
+#         description: params[:description],
+#         rating: params[:rating]
+#         )
+#         redirect "/movies/#{@movie.id}"
+#       else
+#         redirect '/movies/new'
+#       end
+#   else
+#     redirect '/login'
+#   end
+# end
+
+# post '/movies' do
+#   if logged_in?
+#     @movie = current_user.movies.create(
+#       title: params[:title],
+#       genre: params[:genre],
+#       release_date: params[:release_date],
+#       description: params[:description],
+#       rating: params[:rating]
+#       )
+#     if @movie.valid?
+#       redirect "/movies/#{@movie.id}"
+#     else
+#       redirect '/movies/new'
+#     end
+#   else
+#     redirect '/login'
+#   end
+# end
