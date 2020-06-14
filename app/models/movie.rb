12404553a3ca28bc2999e5751970025eaf5a93b8
Alex Velasquez <jtinnesco@gmail.com>
2020-06-14 06:21:07 +0000

    remove valid_params?(params) method from Movie model, relying on activerecord methods to validate user inputs instead.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,9 +1,4 @@
 class Movie < ActiveRecord::Base
   belongs_to :user
-
   validates :title, :genre, :release_date, :description, :rating, presence: true
-
-  # def self.valid_params?(params)
-  #   return !params[:title].empty? && !params[:genre].empty? && !params[:release_date].empty? && !params[:description].empty? && !params[:rating].empty?
-  # end
 end
