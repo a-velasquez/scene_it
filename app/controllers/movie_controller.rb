fb9346ff07d6c5fa595c21b7152531a1c3b62f74
Alex Velasquez <jtinnesco@gmail.com>
2020-06-14 05:43:32 +0000

    remove global variable from post /movies route, not necessary

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -11,15 +11,15 @@ class MovieController < ApplicationController
 
   post '/movies' do
     if logged_in?
-      @movie = current_user.movies.create(
+      movie = current_user.movies.create(
         title: params[:title],
         genre: params[:genre],
         release_date: params[:release_date],
         description: params[:description],
         rating: params[:rating]
         )
-      if @movie.save
-        redirect "/movies/#{@movie.id}"
+      if movie.save
+        redirect "/movies/#{movie.id}"
       else
         redirect '/movies/new'
       end
@@ -110,41 +110,3 @@ class MovieController < ApplicationController
   end
 
 end
-
-# post '/movies' do
-#   if logged_in?
-#     if @movie = Movie.valid_params?(params) #validates user inputs before creating movie
-#       @movie = current_user.movies.create(
-#         title: params[:title],
-#         genre: params[:genre],
-#         release_date: params[:release_date],
-#         description: params[:description],
-#         rating: params[:rating]
-#         )
-#         redirect "/movies/#{@movie.id}"
-#       else
-#         redirect '/movies/new'
-#       end
-#   else
-#     redirect '/login'
-#   end
-# end
-
-# post '/movies' do
-#   if logged_in?
-#     @movie = current_user.movies.create(
-#       title: params[:title],
-#       genre: params[:genre],
-#       release_date: params[:release_date],
-#       description: params[:description],
-#       rating: params[:rating]
-#       )
-#     if @movie.valid?
-#       redirect "/movies/#{@movie.id}"
-#     else
-#       redirect '/movies/new'
-#     end
-#   else
-#     redirect '/login'
-#   end
-# end
