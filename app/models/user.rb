e0734c7fc79ee690d6e5fc7a33b76140f11d0517
Alex Velasquez <jtinnesco@gmail.com>
2020-06-14 06:26:32 +0000

    add activerecord validation of presence of username, email, and password when user signs up for account

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :movies
-
   has_secure_password
   validates_uniqueness_of :username #makes sure that all usernames are unique to prevent duplicates
+  validates :username, :email, :password, presence: true
 end
