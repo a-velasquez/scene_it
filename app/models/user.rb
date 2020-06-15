140786a6aa523ef8eff6caf8c3c6bde6c9340a3b
Alex Velasquez <jtinnesco@gmail.com>
2020-06-15 17:14:50 +0000

    add notes to User model regarding validates activerecord method.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   has_many :movies
   has_secure_password
   validates_uniqueness_of :username #makes sure that all usernames are unique to prevent duplicates
-  validates :username, :email, :password, presence: true
+  validates :username, :email, :password, presence: true #validates user input, make sure fields are not left blank
 end
