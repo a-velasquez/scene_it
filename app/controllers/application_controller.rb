68fab3d8904a8847707c864a577e9beb7c679aae
Alex Velasquez <49491196+a-velasquez@users.noreply.github.com>
2020-06-23 22:43:04 -0700

    add notes to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,26 +6,26 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
     enable :sessions
-    set :session_secret, 'scene'
+    set :session_secret, 'scene' #encryption key used to create session_id
   end
 
   get '/' do
     erb :index
   end
 
-  helpers do
+  helpers do #helpers is a Sinatra method. methods in this block are accessible to views and controllers
 
     def logged_in?
-      !!current_user
+      !!current_user #calls current_user, and returns true or false, based on if a value is true or false. 
     end
 
 
     def current_user
-      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-    end
-
-    def authorized?(movie)
-      movie.user == current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id] #if there is a :user_id key in the session hash
+    end                                                                          #it will look for that user by that session_id and then
+                                                                                 #set the instance variable @current_user equal to that
+    def authorized?(movie)                                                       #user. It will return nil if no user is found.
+      movie.user == current_user #takes movie as an argument and uses comparison operator to check if movie object belongs to the current_user.
     end
 
   end
