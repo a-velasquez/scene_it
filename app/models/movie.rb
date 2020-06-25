bfb8102db5bb67140fa29379ef919e21bf9444ca
Alex Velasquez <49491196+a-velasquez@users.noreply.github.com>
2020-06-24 20:52:41 -0700

    add notes.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,4 +1,4 @@
 class Movie < ActiveRecord::Base
-  belongs_to :user
-  validates :title, :genre, :release_date, :description, :rating, presence: true
+  belongs_to :user. # foreign key - user_id
+  validates :title, :genre, :release_date, :description, :rating, presence: true #uses ActiveRecord Class Method to check that the specified attributes are not blank
 end
