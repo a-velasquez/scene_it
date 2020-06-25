ce8670897994e000af00f762a67351595d57bf7b
Alex Velasquez <49491196+a-velasquez@users.noreply.github.com>
2020-06-24 20:55:43 -0700

    add notes.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
-  has_many :movies
-  has_secure_password
+  has_many :movies #ActiveRecord association 
+  has_secure_password #works with bcrypt gem to 'salt' users password
   validates_uniqueness_of :username #makes sure that all usernames are unique to prevent duplicates
   validates :username, :email, :password, presence: true #validates user input, make sure fields are not left blank
 end
