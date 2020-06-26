a6b117b2340aa61fa8a22bc6585800847ebbfec0
Alex Velasquez <jtinnesco@gmail.com>
2020-06-26 21:06:01 +0000

    format table

--- db/migrate/20200531040513_create_movies_table.rb
+++ db/migrate/20200531040513_create_movies_table.rb
@@ -1,13 +1,13 @@
 class CreateMoviesTable < ActiveRecord::Migration
   def change
     create_table :movies do |t|
-      t.string :title
-      t.string :genre
+      t.string  :title
+      t.string  :genre
       t.integer :release_date
-      t.text :description
+      t.text    :description
       t.integer :rating
       t.integer :user_id
-      
+
       t.timestamps
     end
   end
