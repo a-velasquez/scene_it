5bf3d9b642bb755f6b59455ffd66102b193b096f
Alex Velasquez <jtinnesco@gmail.com>
2020-06-26 21:31:28 +0000

    create categories table and migrate

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200626211855) do
+ActiveRecord::Schema.define(version: 20200626212430) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+  end
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
