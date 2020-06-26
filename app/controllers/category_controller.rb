6a3ea89b72ac9097bba66009da8e6b93154b6947
Alex Velasquez <jtinnesco@gmail.com>
2020-06-26 23:53:52 +0000

    add get categories and get categories/:id and private get_category route.

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,4 +1,23 @@
 class CategoryController < ApplicationController
-  
+
+
+#show all
+  get '/categories' do
+    @categories = Category.all
+    erb :'categories/index'
+  end
+
+  #show one
+
+  get '/categories/:id' do
+    get_category
+    erb :'categories/show'
+  end
+
+  private
+
+  def get_category
+    @category = Category.find_by_id(params[:id])
+  end
 
 end
