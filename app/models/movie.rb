c38e02967dc1f1b8f1e9d2b97b0e94c55a21fbcc
Alex Velasquez <jtinnesco@gmail.com>
2020-06-27 23:18:00 +0000

    re-arranged class associations.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ActiveRecord::Base
   belongs_to :user # foreign key - user_id
-  belongs_to :category
+  has_many :movie_categories
+  has_many  :categories, through: :movie_categories
+
   validates :title, :release_date, :description, :rating, presence: true #uses ActiveRecord Class Method to check that the specified attributes are not blank
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,8 @@
 class User < ActiveRecord::Base
-  has_many :categories, :dependent => :destroy
+  # has_many :categories, :dependent => :destroy
   has_many :movies
-  
+  # has_many :categories, through: :movie_categories
+
   has_secure_password #works with bcrypt gem to 'salt' users password
   validates_uniqueness_of :username #makes sure that all usernames are unique to prevent duplicates
   validates :username, :email, :password, presence: true #validates user input, make sure fields are not left blank
