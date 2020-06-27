001e05008452d47c548babeeed8437d284eedf23
Alex Velasquez <jtinnesco@gmail.com>
2020-06-27 04:19:23 +0000

    Category.first.name returns name of category.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ActiveRecord::Base
- has_many :movies
+ has_many :movies, :dependent => :destroy
  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :categories, :dependent => :destroy
   has_many :movies
-  has_many :categories
-
+  
   has_secure_password #works with bcrypt gem to 'salt' users password
   validates_uniqueness_of :username #makes sure that all usernames are unique to prevent duplicates
   validates :username, :email, :password, presence: true #validates user input, make sure fields are not left blank
