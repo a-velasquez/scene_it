ca12d9185eb294e311c55dc56a60f243f55f27f2
Alex Velasquez <jtinnesco@gmail.com>
2020-06-27 00:35:32 +0000

    removed :genre from validations.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,5 +2,5 @@ class Movie < ActiveRecord::Base
   belongs_to :user # foreign key - user_id
   has_many :movie_categories
   has_many :categories, through: :movie_categories
-  validates :title, :genre, :release_date, :description, :rating, presence: true #uses ActiveRecord Class Method to check that the specified attributes are not blank
+  validates :title, :release_date, :description, :rating, presence: true #uses ActiveRecord Class Method to check that the specified attributes are not blank
 end
