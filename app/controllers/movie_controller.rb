33417ddc8b038260651f5ee738b6ad6805fdadaf
Alex Velasquez <jtinnesco@gmail.com>
2020-06-27 01:18:13 +0000

    remove genre column from movies table.

--- app/controllers/movie_controller.rb
+++ app/controllers/movie_controller.rb
@@ -1,9 +1,10 @@
 class MovieController < ApplicationController
 
   #CREATE MOVIE
-  
+
   get '/movies/new' do
     if logged_in?
+      get_categories
       erb :'/movies/new'
     else
       redirect '/login'
@@ -12,14 +13,15 @@ class MovieController < ApplicationController
 
   post '/movies' do
     if logged_in?
-      movie = current_user.movies.create( #uses ActiveRecord association collection method to instantiate new instance of movie associated 
-        title: params[:title],            #with the current_user's foreign key. Also triggers validations using .create
-        genre: params[:genre],
+      binding.pry
+      movie = current_user.movies.create(
+        title: params[:title],
+        category_id: params[:category],
         release_date: params[:release_date],
         description: params[:description],
         rating: params[:rating]
         )
-      if movie.save                       #if movie is saved (and passes validations) redirect to individual movie
+      if movie.save
         redirect "/movies/#{movie.id}"
       else
         redirect '/movies/new'
@@ -30,7 +32,7 @@ class MovieController < ApplicationController
   end
 
   #READ MOVIE
-  
+
   get '/movies' do
     if logged_in?
       @movies = current_user.movies #Association collection method to query current_user's movies
@@ -41,9 +43,9 @@ class MovieController < ApplicationController
   end
 
   get '/movies/:id' do
-    get_movie                #uses private method get_movie to find movie by id
+    get_movie
     if logged_in?
-      if authorized?(@movie) #uses helper method to check if particualar movie belongs to user
+      if authorized?(@movie)
         erb :'movies/show'
       else
         redirect '/movies'
@@ -54,11 +56,11 @@ class MovieController < ApplicationController
   end
 
   #UPDATE MOVIE
-  
+
   get '/movies/:id/edit' do
     if logged_in?
       get_movie
-      if authorized?(@movie) #ensures user can't modify movies saved by others
+      if authorized?(@movie)
         erb :'movies/edit'
       else
         redirect '/movies'
@@ -89,12 +91,12 @@ class MovieController < ApplicationController
 
 
   #DELETE MOVIE
-  
+
   get '/movies/:id/delete' do
     if logged_in?
       get_movie
-        if authorized?(@movie) #ensures movie belongs to current user
-          @movie.destroy       #clears db of all records associated with movie object
+        if authorized?(@movie)
+          @movie.destroy
           redirect '/movies'
         else
           redirect '/movies'
@@ -108,6 +110,10 @@ class MovieController < ApplicationController
 
   def get_movie
     @movie = Movie.find_by(id: params[:id])
+  end
+
+  def get_categories
+    @categories = Category.all
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200626221514) do
+ActiveRecord::Schema.define(version: 20200627011640) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20200626221514) do
 
   create_table "movies", force: :cascade do |t|
     t.string   "title"
-    t.string   "genre"
     t.integer  "release_date"
     t.text     "description"
     t.integer  "rating"
