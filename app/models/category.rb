840e9279da6f791a6644d82844b7e6540d3e26cf
Alex Velasquez <jtinnesco@gmail.com>
2020-06-29 02:59:11 +0000

    make Category titles links to individual category show page.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
  has_many :movie_categories
  has_many :movies, through: :movie_categories
- has_many :users, through: :movies
+ has_many :users, through: :movie_categories
 end
