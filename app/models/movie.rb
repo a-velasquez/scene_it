fcedea27a96d135068a2107fd941eef5da5caf15
Alex Velasquez <jtinnesco@gmail.com>
2020-06-30 18:15:17 +0000

    change :categories to :category in Movie model.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,6 @@
 class Movie < ActiveRecord::Base
-  belongs_to :user 
-  belongs_to :categories
+  belongs_to :user
+  belongs_to :category
 
   validates :title, :release_date, :description, :rating, presence: true #uses ActiveRecord Class Method to check that the specified attributes are not blank
 end
