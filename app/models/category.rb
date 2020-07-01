ff21270e7804aef229bd61cbebe2b42d8333f404
Alex Velasquez <jtinnesco@gmail.com>
2020-07-01 01:08:52 +0000

    add dependent destroy to category model to delete movies associated with category when category deleted.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,6 @@
 class Category < ActiveRecord::Base
- has_many :movies
+ has_many :movies, dependent: :destroy
  has_many :users, through: :movies
 
- validates :name, uniqueness: true 
+ validates :name, uniqueness: true
 end
